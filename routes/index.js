99dfba05fbb81a3e065f1c5b7020dbd07e6f27c0
Akash Raveendran <akashraveendran@gmail.com>
2022-03-28 19:54:28 +0530

    added all the routes and controllers for user

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-const {getUserLogin,getUserSignup, createNewUser, doUserLogin, getHomePage} = require("../controllers/user-controller")
+const { getUserLogin,
+    getUserSignup,
+    createNewUser,
+    doUserLogin,
+    getHomePage,
+    getJobsPage,
+    getAllCompanies,
+    getJobApplicationForm,
+    applyJob,
+    getUserNotifications,
+    getUserApplications
+} = require("../controllers/user-controller")
 
-/* GET home page. */
-router.get('/',getHomePage);
 
 router.route('/login').get(getUserLogin).post(doUserLogin)
-router.route('/signup').get(getUserSignup).post(createNewUser)
+router.route('/signup').get(getUserSignup).post(createNewUser);
+router.route('/').get(getHomePage);
+router.route('/alljobs').get(getJobsPage);
+router.route('/companies').get(getAllCompanies);
+router.route('/apply-job/:id').get(getJobApplicationForm).post(applyJob);
+router.route('/notifications/:id').get(getUserNotifications);
+router.route('/user-applications/:id').get(getUserApplications);
+
+
+
 
 module.exports = router;
