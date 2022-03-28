fd83637b47ea7c2c1abb578c949e69118a5ae68e
Akash Raveendran <akashraveendran@gmail.com>
2022-03-28 13:06:18 +0530

    Added .env file and mongo connection string in .env

--- app.js
+++ app.js
@@ -3,26 +3,26 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const connectDB = require("./db/connection")
+const connectDB = require("./db/connection");
+require("dotenv").config();
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-  
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-async function connect(){
+async function connect() {
   try {
-    let url = "mongodb+srv://user01:1234@ecommerceweb.3j3ck.mongodb.net/job-portal?retryWrites=true&w=majority"
+    let url = process.env.MONGO_URI; //connectionn string from .env file
     await connectDB(url);
     console.log("DB connected");
   } catch (error) {
@@ -32,15 +32,14 @@ async function connect(){
 connect();
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
