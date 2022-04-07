ae13bd4a2ffb2d143dd56e529e32f382ba0a5d0d
Akash Raveendran <akashraveendran@gmail.com>
2022-04-07 11:22:08 +0530

    added alert message to the checkuser

--- middleware/checkUser.js
+++ middleware/checkUser.js
@@ -1,5 +1,6 @@
 const checkUser = function (req, res, next) {
     if (!req.session.user) {
+        req.session.alertMessage = "Please Login to Continue"
         res.redirect('/login');
     } else {
         next();
